77552ac610550ad15873caa27b272f9397a66e84
murat <murat@LAPTOP-K3E7VS6K>
2024-01-09 14:13:19 +0300

    Add project files.

---
+++ odev2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace odev2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("Başlangıç Sayısı:");
+            byte bas = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Bitiş Sayısı:");
+            byte bit = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sayı Adeti:");
+            byte adet = byte.Parse(Console.ReadLine());
+
+
+            int[] numbers = new int[adet];
+            int num;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                num = rnd.Next(bas, bit);
+                if (i == 0)
+                {
+                    numbers[i] = num;
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[j] == num)
+                    {
+                        i--;
+                        break;
+                    }
+                    else
+                    {
+                        numbers[i] = num;
+                    }
+                }
+            }
+            Array.Sort(numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
